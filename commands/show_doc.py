da04e6d462f2ad056feae1fc5eb7ad3c331a220e
Zhengbo Li <zhengbli@microsoft.com>
2015-05-21 18:38:23 -0700

    Moving files under the same top level folder for importing

--- commands/show_doc.py
+++
@@ -1,7 +0,0 @@
-from .base_command import TypeScriptBaseTextCommand
-
-
-class TypescriptShowDoc(TypeScriptBaseTextCommand):
-    def run(self, text, info_str="", doc_str=""):
-        self.view.insert(text, self.view.sel()[0].begin(), info_str + "\n\n")
-        self.view.insert(text, self.view.sel()[0].begin(), doc_str)
--- listeners/__init__.py
+++
@@ -1,1 +0,0 @@
-from .listeners import TypeScriptEventListener
