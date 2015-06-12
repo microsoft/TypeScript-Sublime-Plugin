1ede13b7770c3f254e2ba480846f16d3c56a216b
Zhengbo Li <zhengbli@microsoft.com>
2015-06-12 14:35:15 -0700

    CR feedback

--- typescript/commands/build.py
+++ typescript/commands/build.py
@@ -1,4 +1,4 @@
-import sublime_plugin
+﻿import sublime_plugin
 
 from ..libs.global_vars import *
 from ..libs import cli
@@ -7,7 +7,7 @@ from ..libs import cli
 class TypescriptBuildCommand(sublime_plugin.WindowCommand):
     def run(self):
         if get_node_path() is None:
-            print("Cannot found node. Build cancelled.")
+            print("Cannot find node. Build cancelled.")
             return
 
         file_name = self.window.active_view().file_name()
@@ -17,19 +17,19 @@ class TypescriptBuildCommand(sublime_plugin.WindowCommand):
                 tsconfig_dir = dirname(project_info["body"]["configFileName"])
                 self.window.run_command("exec", {
                     "cmd": [get_node_path(), TSC_PATH, "-p", tsconfig_dir],
+                    # regex to capture build result for displaying in the output panel
                     "file_regex": "^(.+?)\\((\\d+),(\\d+)\\): (.+)$"
                 })
             else:
                 sublime.active_window().show_input_panel(
                     "Build parameters: ",
                     "",  # initial text
-                    self.compile_inferred_project,
+                    lambda params: self.compile_inferred_project(file_name, params),
                     None,  # on change
                     None   # on cancel
                 )
 
-    def compile_inferred_project(self, params=""):
-        file_name = self.window.active_view().file_name()
+    def compile_inferred_project(self, file_name, params=""):
         cmd = [get_node_path(), TSC_PATH, file_name]
         print(cmd)
         if params != "":
