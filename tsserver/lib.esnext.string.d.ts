8833e5a71145ca106e51d1e4169d987c31e98290
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2020-05-11 22:37:38 -0700

    Update to TypeScript 3.9.2.

--- tsserver/lib.esnext.string.d.ts
+++ tsserver/lib.esnext.string.d.ts
@@ -18,18 +18,18 @@ and limitations under the License.
 /// <reference no-default-lib="true"/>
 
 
-/// <reference lib="es2015.iterable" />
-
-interface ObjectConstructor {
+interface String {
     /**
-     * Returns an object created by key-value entries for properties and methods
-     * @param entries An iterable object that contains key-value entries for properties and methods.
+     * Replace all instances of a substring in a string, using a regular expression or search string.
+     * @param searchValue A string to search for.
+     * @param replaceValue A string containing the text to replace for every successful match of searchValue in this string.
      */
-    fromEntries<T = any>(entries: Iterable<readonly [PropertyKey, T]>): { [k: string]: T };
+    replaceAll(searchValue: string | RegExp, replaceValue: string): string;
 
     /**
-     * Returns an object created by key-value entries for properties and methods
-     * @param entries An iterable object that contains key-value entries for properties and methods.
+     * Replace all instances of a substring in a string, using a regular expression or search string.
+     * @param searchValue A string to search for.
+     * @param replacer A function that returns the replacement text.
      */
-    fromEntries(entries: Iterable<readonly any[]>): any;
+    replaceAll(searchValue: string | RegExp, replacer: (substring: string, ...args: any[]) => string): string;
 }
